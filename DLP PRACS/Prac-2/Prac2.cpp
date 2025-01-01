66cb69a0dfb094ee9edfb77e4b0581eb82a74893
Mohit1684 <139526246+Mohit1684@users.noreply.github.com>
2025-01-01 15:44:42 +0530

    Update Prac2.cpp

--- DLP PRACS/Prac-2/Prac2.cpp
+++ DLP PRACS/Prac-2/Prac2.cpp
@@ -1,75 +1,52 @@
-#include<iostream>
-#include<string>
-
+#include <iostream>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    int A[] = {1,2,3,4};
-    int i=0;
-    int f=0;
-    int c=0;
-    string S;
-    cout<<"Enter the Initial State: ";
-    cin>>i;
-    cout<<"Enter the Final State: ";
-    cin>>f;
-    cout<<"Enter the String: ";
-    cin>>S;
-    int n=S.size();
+    int inSymbol, state, currState, finalState;
+    
+    cout << "Enter the number of Input Symbols: ";
+    cin >> inSymbol;
+    vector<char> symbols(inSymbol);
+    cout << "Enter the Symbols: ";
+    for (char &symbol : symbols)
+        cin >> symbol;
 
-    if(S[0]=='a'){
-        c=A[1];
-    }
-    else if(S[0]=='b'){
-        c=A[2];
-    }
+    cout << "Enter the number of States: ";
+    cin >> state;
+    vector<char> states(state);
+    cout << "Enter the States: ";
+    for (char &stateChar : states)
+        cin >> stateChar;
 
-    else if(S[1]=='a'){
-        c=A[0];
-    }
-    else if(S[1]=='b'){
-        c=A[3];
-    }
+    cout << "Enter the Initial State: ";
+    cin >> currState;
+    cout << "Enter the Final State: ";
+    cin >> finalState;
 
-    else if(S[2]=='a'){
-        c=A[3];
-    }
-    else if(S[2]=='b'){
-        c=A[0];
-    }
-
-    else if(S[3]=='a'){
-        c=A[2];
-    }
-    else if(S[3]=='b'){
-        c=A[1];
+    unordered_map<string, int> table;
+    for (const char &stateChar : states)
+    {
+        for (const char &symbol : symbols)
+        {
+            int nextState;
+            cout << stateChar << " -> " << symbol << " ";
+            cin >> nextState;
+            table[string(1, stateChar) + symbol] = nextState;
+        }
     }
 
-    if(c==f){
-        cout<<"Valid String";
+    string inpString;
+    cout << "Enter input String: ";
+    cin >> inpString;
+
+    for (char inputChar : inpString)
+    {
+        currState = table[string(1, currState) + inputChar];
     }
-    else{
-        cout<<"Invalid String";
-    }
 
-
-
+    cout << (currState == finalState ? "String is Valid" : "String is Invalid");
     return 0;
 }
-
-
-/*
-1 to a = 2
-1 to b = 3
-2 to a = 1
-2 to b = 4
-3 to a = 4
-3 to b = 1
-4 to a = 3
-4 to b = 2
-  a    b    b    a    b    a    b
-(1-2)(2-4)(4-2)(2-1)(1-3)(3-4)(4-2)
-*/
-
-
